a225903845c870a71d93f7b926b8a8ad46a4770a
uomma <marcelloleone975@gmail.com>
2023-03-03 16:11:30 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+'use strict'
+
+/* FUNCTIONS */
+
+
+function mioCreaElement(htmlElement, className, htmlValue) {
+    const element = document.createElement(htmlElement);
+    element.classList.add(className);
+    element.innerText = htmlValue;
+
+    element.addEventListener('click', function(){
+
+        alert(htmlValue);
+    })
+    return element;
+}
+
+
+function myAppendElement(containerElement, htmlElement) {
+    containerElement.append(htmlElement);
+}
+
+/* MAIN */
+let cellNumber = 100;
+
+const containerGrid = document.querySelector('.grid');
+
+for(let i = 1; i <= cellNumber; i++){
+
+    const createdElement = mioCreaElement('div', 'celle-10', i);
+    myAppendElement(containerGrid, createdElement);
+
+}
